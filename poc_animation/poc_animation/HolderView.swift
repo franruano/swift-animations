0c357bd81b000cee548b91254f7192a2bbc010f9
Suarez, Francisco (iOS Developer) <Francisco.Suarez@sky.uk>
2015-09-09 08:46:49 +0100

    Anhadidas mas pelotas

--- poc_animation/poc_animation/HolderView.swift
+++ poc_animation/poc_animation/HolderView.swift
@@ -29,6 +29,16 @@ class HolderView: UIView {
         let oval6 = ovalCoordenates(initialRect: CGRect(x: 105.5, y: 270.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 105.5, y: 270.0, width: 25.0, height: 25.0), delay: 1.45)
         let oval7 = ovalCoordenates(initialRect: CGRect(x: 110.5, y: 190.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 110.5, y: 190.0, width: 25.0, height: 25.0), delay: 1.45)
         let oval8 = ovalCoordenates(initialRect: CGRect(x: 105.5, y: 150.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 105.5, y: 150.0, width: 25.0, height: 25.0), delay: 1.45)
+        let oval9 = ovalCoordenates(initialRect: CGRect(x: 75.5, y: 170.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 75.5, y: 170.0, width: 25.0, height: 25.0), delay: 1.45)
+        let oval10 = ovalCoordenates(initialRect: CGRect(x: 120.5, y: 220.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 120.5, y: 220.0, width: 25.0, height: 25.0), delay: 1.45)
+        let oval11 = ovalCoordenates(initialRect: CGRect(x: 150.5, y: 250.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 150.5, y: 250.0, width: 25.0, height: 25.0), delay: 1.45)
+        
+        let oval12 = ovalCoordenates(initialRect: CGRect(x: 145.5, y: 110.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 145.5, y: 110.0, width: 25.0, height: 25.0), delay: 1.45)
+        let oval13 = ovalCoordenates(initialRect: CGRect(x: 140.5, y: 140.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 140.5, y: 140.0, width: 25.0, height: 25.0), delay: 1.45)
+        let oval14 = ovalCoordenates(initialRect: CGRect(x: 160.5, y: 180.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 160.5, y: 180.0, width: 25.0, height: 25.0), delay: 1.45)
+        let oval15 = ovalCoordenates(initialRect: CGRect(x: 180.5, y: 210.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 180.5, y: 210.0, width: 25.0, height: 25.0), delay: 1.45)
+        let oval16 = ovalCoordenates(initialRect: CGRect(x: 215.5, y: 240.0, width: 0.0, height: 0.0), finalRect: CGRect(x: 215.5, y: 240.0, width: 25.0, height: 25.0), delay: 1.45)
+        
         
         _arrOvalCoordenates.append(oval0)
         _arrOvalCoordenates.append(oval1)
@@ -39,6 +49,14 @@ class HolderView: UIView {
         _arrOvalCoordenates.append(oval6)
         _arrOvalCoordenates.append(oval7)
         _arrOvalCoordenates.append(oval8)
+        _arrOvalCoordenates.append(oval9)
+        _arrOvalCoordenates.append(oval10)
+        _arrOvalCoordenates.append(oval11)
+        _arrOvalCoordenates.append(oval12)
+        _arrOvalCoordenates.append(oval13)
+        _arrOvalCoordenates.append(oval14)
+        _arrOvalCoordenates.append(oval15)
+        _arrOvalCoordenates.append(oval16)
         
         return _arrOvalCoordenates
         }()
@@ -97,6 +115,15 @@ class HolderView: UIView {
              NSTimer.scheduledTimerWithTimeInterval(2.55, target: self, selector: "drawCircleAnimation:", userInfo: 7, repeats: false)
             
             NSTimer.scheduledTimerWithTimeInterval(2.75, target: self, selector: "drawCircleAnimation:", userInfo: 8, repeats: false)
+             NSTimer.scheduledTimerWithTimeInterval(2.80, target: self, selector: "drawCircleAnimation:", userInfo: 9, repeats: false)
+            NSTimer.scheduledTimerWithTimeInterval(2.90, target: self, selector: "drawCircleAnimation:", userInfo: 10, repeats: false)
+            NSTimer.scheduledTimerWithTimeInterval(3.00, target: self, selector: "drawCircleAnimation:", userInfo: 11, repeats: false)
+            
+            NSTimer.scheduledTimerWithTimeInterval(4.00, target: self, selector: "drawCircleAnimation:", userInfo: 12, repeats: false)
+             NSTimer.scheduledTimerWithTimeInterval(4.10, target: self, selector: "drawCircleAnimation:", userInfo: 13, repeats: false)
+            NSTimer.scheduledTimerWithTimeInterval(4.15, target: self, selector: "drawCircleAnimation:", userInfo: 14, repeats: false)
+             NSTimer.scheduledTimerWithTimeInterval(4.20, target: self, selector: "drawCircleAnimation:", userInfo: 15, repeats: false)
+            NSTimer.scheduledTimerWithTimeInterval(4.20, target: self, selector: "drawCircleAnimation:", userInfo: 16, repeats: false)
             
         }
     }
