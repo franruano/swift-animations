05ae8821b250127700ebe017cf75c563c2796ea2
Fran Ruano <franruan@gmail.com>
2015-09-21 06:22:51 +0100

    Complete basics

--- poc_animation/poc_animation/HolderView.swift
+++ poc_animation/poc_animation/HolderView.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
+protocol HolderStatusDelegate {
+    func animationExpanded()
+    func animationCompleted()
+}
+
 class HolderView: UIView {
 
     let arcLayer = ArcLayer()
     
-    var redRectangleLayer: RectangleNewLayer?
+    var rectangleLayer: RectangleNewLayer?
     var circleOneLayer: OvalLayer?
     var arrOvalLayers = [OvalLayer]()
-    
+    var delegate:HolderStatusDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,8 +34,8 @@ class HolderView: UIView {
     
     
     func drawSquareWithAnimation() {
-        redRectangleLayer = RectangleNewLayer(size:CGFloat(self.frame.width))
-        if let redRectangleLayer = redRectangleLayer {
+        rectangleLayer = RectangleNewLayer(size:CGFloat(self.frame.width))
+        if let redRectangleLayer = rectangleLayer {
             layer.addSublayer(redRectangleLayer)
             redRectangleLayer.animateStrokeWithColor(UIColor.blackColor())
         }
@@ -130,7 +135,7 @@ class HolderView: UIView {
                 NSTimer.scheduledTimerWithTimeInterval(initialTime, target: self, selector: "dropCircleAnimation:", userInfo: i, repeats: false)
             }
             //Round 4
-            time = 7.75
+            time = 7.5
             for i in 18...32 {
                 NSTimer.scheduledTimerWithTimeInterval(time, target: self, selector: "dropCircleAnimation:", userInfo: i, repeats: false)
             }
@@ -138,6 +143,77 @@ class HolderView: UIView {
 
         NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: "drawArc",
             userInfo: nil, repeats: false)
+        
+        NSTimer.scheduledTimerWithTimeInterval(8.0, target: self, selector: "expandView:",
+            userInfo: false, repeats: false)
+        NSTimer.scheduledTimerWithTimeInterval(8.5, target: self, selector: "expandView:",
+            userInfo: true, repeats: false)
+        NSTimer.scheduledTimerWithTimeInterval(9.0, target: self, selector: "contractView",
+            userInfo: nil, repeats: false)
     }
+    
+    
+    func expandView(timerIsVertical: NSTimer) {
+        backgroundColor = UIColor.blackColor()
+        if let rectangleLayer = rectangleLayer {
+
+        var frameInternal: CGRect?
+        if let frameParent = self.superview?.frame, let timerIsVertical = timerIsVertical.userInfo as? Bool {
+                if !timerIsVertical {
+                    frameInternal = CGRectMake(0,
+                        self.frame.origin.y,
+                        frameParent.size.width,
+                        self.frame.size.height)
+                } else {
+                    frameInternal = CGRectMake(self.frame.origin.x,
+                        0,
+                        self.frame.size.width,
+                        frameParent.size.height)
+                }
+            }
+            
+        layer.sublayers = nil
+        
+        UIView.animateWithDuration(0.2, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: {
+                if let frameInternal = frameInternal {
+                    self.frame = frameInternal
+                }
+                
+            }, completion: { finished in
+                delegate?.animationExpanded()
+            })
+        }
+    }
+    
+    func contractView() {
+        if let rectangleLayer = rectangleLayer {
+            
+            var frameInternal: CGRect?
+            if let frameParent = self.superview?.frame{
+                    frameInternal = CGRectMake(self.frame.origin.x,
+                        self.frame.origin.y,
+                        0,
+                        0)
+            }
+            
+            layer.sublayers = nil
+            
+            UIView.animateWithDuration(0.2, delay: 0.0, options: UIViewAnimationOptions.CurveEaseInOut,
+                animations: {
+                    if let frameInternal = frameInternal {
+                        self.frame = frameInternal
+                        if let parentView = self.superview {
+                            self.center = parentView.center
+                        }
+                    }
+                    
+                }, completion: { finished in
+                    delegate?.animationCompleted()
+            })
+        }
+
+    }
+    
 
 }
--- poc_animation/poc_animation/ViewController.swift
+++ poc_animation/poc_animation/ViewController.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, HolderStatusDelegate {
 
-//    private struct Constants {
-        let leftPadding = CGFloat(20.0)
-//    }
-    var holder: HolderView?
+    let leftPadding = CGFloat(20.0)
     
+    var holder: HolderView?
+    var iconView: UIImageView?
     
     
     
@@ -29,12 +28,49 @@ class ViewController: UIViewController {
         let frame = self.view.frame
         let sideLength = frame.width - 2*leftPadding
         
+        iconView = UIImageView (frame: CGRect(x: (frame.width - sideLength)/2, y: (frame.height - sideLength)/2, width: sideLength - leftPadding, height: sideLength - leftPadding))
+        iconView?.center = self.view.center
+        iconView?.backgroundColor = UIColor.blueColor()
+        iconView?.hidden = true
+        self.view.addSubview(iconView!)
+        
         holder = HolderView (frame: CGRect(x: (frame.width - sideLength)/2, y: (frame.height - sideLength)/2, width: sideLength, height: sideLength))
+        holder?.delegate = self
         self.view.addSubview(holder!)
         
         holder!.addSquareBorder()
         holder!.addCircle()
     }
+    
+    //MARK: - Holder Delegate
+    func animationExpanded() {
+        iconView?.hidden = false
+    }
+    
+    func animationCompleted() {
+        holder!.removeFromSuperview()
+        holder = nil
+        var label: UILabel
+        if let iconView = iconView {
+            label = UILabel(frame: CGRect(x: iconView.frame.origin.x, y: iconView.frame.origin.y + iconView.frame.height+leftPadding, width: iconView.frame.width, height: 30))
+            
+            label.textColor = UIColor.blackColor()
+            label.font = UIFont.boldSystemFontOfSize(30)
+            label.textAlignment = NSTextAlignment.Center
+            label.text = "My Audio Diary"
+            label.transform = CGAffineTransformScale(label.transform, 0.25, 0.25)
+            label.sizeToFit()
+            view.addSubview(label)
+            UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveEaseInOut,
+                animations: ({
+                    label.transform = CGAffineTransformScale(label.transform, 4.0, 4.0)
+                }), completion: { finished in
+            
+            })
+        }
+    }
+    
+    
     
    }
 
